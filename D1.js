cd89b68606260ee968302e3392e9c06b077fc1ad
Paolo <pascuccip9@gmail.com>
2023-10-02 17:44:43 +0200

    .

--- D1.js
+++ D1.js
@@ -79,5 +79,5 @@ console.log(x - 4);
 let name1 = "jhon";
 let name2 = "Jhon";
 
-console.log(name1 === name2);
-console.log(name1 === name2.toLowerCase());
+console.log(name1 !== name2);
+console.log(name1.toLowerCase() === name2.toLowerCase());
